c5649999d33a79434abf9d7b9f35aa4c37e0abca
redonkulus <seth@bertalotto.net>
2024-12-09 18:26:19 +0000

    Deploy website - based on 63e82d837e4e87d2e58dce7e64fc34c6de516aa0

--- js/menus.js
+++ js/menus.js
@@ -1,1 +1,172 @@
-!function(e,t){"use strict";function n(n){var i="pure-menu-active",r="role",u="aria-hidden",s=".pure-menu-link",o=e.hasOwnProperty&&e.hasOwnProperty("ontouchstart")?"touchstart":"mousedown",l=this;this._state=1,this.show=function(){0!==this._state&&(this._dropdownParent.classList.add(i),this._menu.setAttribute(u,!1),this._state=0)},this.hide=function(){1!==this._state&&(this._dropdownParent.classList.remove(i),this._menu.setAttribute(u,!0),this._link.focus(),this._state=1)},this.toggle=function(){this[1===this._state?"show":"hide"]()},this.halt=function(e){e.stopPropagation(),e.preventDefault()},this._dropdownParent=n,this._link=this._dropdownParent.querySelector(s),this._menu=this._dropdownParent.querySelector(".pure-menu-children"),this._firstMenuLink=this._menu.querySelector(s),this._link.setAttribute("aria-haspopup","true"),this._menu.setAttribute(r,"menu"),this._menu.setAttribute("aria-labelledby",this._link.getAttribute("id")),this._menu.setAttribute("aria-hidden","true"),[].forEach.call(this._menu.querySelectorAll("li"),(function(e){e.setAttribute(r,"presentation")})),[].forEach.call(this._menu.querySelectorAll("a"),(function(e){e.setAttribute(r,"menuitem")})),this._link.addEventListener("click",(function(e){e.stopPropagation(),e.preventDefault(),l.toggle()})),t.addEventListener("keydown",(function(e){var t,n,i,r,u;if(0===l._state&&!l._menu.querySelector(".pure-menu-active"))if(t=l._menu.querySelector(":focus"),27===e.keyCode)l.halt(e),l.hide();else if(40===e.keyCode){for(l.halt(e),i=t?t.parentNode.nextSibling:null;i&&1!==i.nodeType;)i=i.nextSibling;u=i?i.querySelector(".pure-menu-link"):null,t?u&&u.focus():l._menu.querySelector(".pure-menu-link").focus()}else if(38===e.keyCode){for(l.halt(e),n=t?t.parentNode.previousSibling:null;n&&1!==n.nodeType;)n=n.previousSibling;r=n?n.querySelector(".pure-menu-link"):null,t?r&&r.focus():l._menu.querySelector(".pure-menu-item:last-child .pure-menu-link").focus()}})),t.addEventListener(o,(function(e){var t=e.target;t===l._link||l._menu.contains(t)||(l.hide(),l._link.blur())}))}!function(){for(var e=t.querySelectorAll(".pure-menu-has-children"),i=0;i<e.length;i++)new n(e[i])}()}(this,this.document);
+/* eslint no-unused-vars:0 no-prototype-builtins:0 */
+(function (window, document) {
+    'use strict';
+
+    // Enable drop-down menus in Pure
+    // Inspired by YUI3 gallery-simple-menu by Julien LeComte
+    // [https://github.com/yui/yui3-gallery/blob/master/src/gallery-simple-menu/js/simple-menu.js]
+
+    function PureDropdown(dropdownParent) {
+
+        var PREFIX = 'pure-',
+            ACTIVE_CLASS_NAME = PREFIX + 'menu-active',
+            ARIA_ROLE = 'role',
+            ARIA_HIDDEN = 'aria-hidden',
+            MENU_OPEN = 0,
+            MENU_CLOSED = 1,
+            MENU_ACTIVE_SELECTOR = '.pure-menu-active',
+            MENU_LINK_SELECTOR = '.pure-menu-link',
+            MENU_SELECTOR = '.pure-menu-children',
+            DISMISS_EVENT = (window.hasOwnProperty &&
+                window.hasOwnProperty('ontouchstart')) ?
+                'touchstart' : 'mousedown',
+
+            ARROW_KEYS_ENABLED = true,
+
+            ddm = this; // drop down menu
+
+        this._state = MENU_CLOSED;
+
+        this.show = function () {
+            if (this._state !== MENU_OPEN) {
+                this._dropdownParent.classList.add(ACTIVE_CLASS_NAME);
+                this._menu.setAttribute(ARIA_HIDDEN, false);
+                this._state = MENU_OPEN;
+            }
+        };
+
+        this.hide = function () {
+            if (this._state !== MENU_CLOSED) {
+                this._dropdownParent.classList.remove(ACTIVE_CLASS_NAME);
+                this._menu.setAttribute(ARIA_HIDDEN, true);
+                this._link.focus();
+                this._state = MENU_CLOSED;
+            }
+        };
+
+        this.toggle = function () {
+            this[this._state === MENU_CLOSED ? 'show' : 'hide']();
+        };
+
+        this.halt = function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+        };
+
+        this._dropdownParent = dropdownParent;
+        this._link = this._dropdownParent.querySelector(MENU_LINK_SELECTOR);
+        this._menu = this._dropdownParent.querySelector(MENU_SELECTOR);
+        this._firstMenuLink = this._menu.querySelector(MENU_LINK_SELECTOR);
+
+        // Set ARIA attributes
+        this._link.setAttribute('aria-haspopup', 'true');
+        this._menu.setAttribute(ARIA_ROLE, 'menu');
+        this._menu.setAttribute('aria-labelledby', this._link.getAttribute('id'));
+        this._menu.setAttribute('aria-hidden', 'true');
+        [].forEach.call(
+            this._menu.querySelectorAll('li'),
+            function(el){
+                el.setAttribute(ARIA_ROLE, 'presentation');
+            }
+        );
+        [].forEach.call(
+            this._menu.querySelectorAll('a'),
+            function(el){
+                el.setAttribute(ARIA_ROLE, 'menuitem');
+            }
+        );
+
+        // Toggle on click
+        this._link.addEventListener('click', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            ddm.toggle();
+        });
+
+        // Keyboard navigation
+        document.addEventListener('keydown', function (e) {
+            var currentLink,
+                previousSibling,
+                nextSibling,
+                previousLink,
+                nextLink;
+
+            // if the menu isn't active, ignore
+            if (ddm._state !== MENU_OPEN) {
+                return;
+            }
+
+            // if the menu is the parent of an open, active submenu, ignore
+            if (ddm._menu.querySelector(MENU_ACTIVE_SELECTOR)) {
+                return;
+            }
+
+            currentLink = ddm._menu.querySelector(':focus');
+
+            // Dismiss an open menu on ESC
+            if (e.keyCode === 27) {
+                /* Esc */
+                ddm.halt(e);
+                ddm.hide();
+            }
+            // Go to the next link on down arrow
+            else if (ARROW_KEYS_ENABLED && e.keyCode === 40) {
+                /* Down arrow */
+                ddm.halt(e);
+                // get the nextSibling (an LI) of the current link's LI
+                nextSibling = (currentLink) ? currentLink.parentNode.nextSibling : null;
+                // if the nextSibling is a text node (not an element), go to the next one
+                while (nextSibling && nextSibling.nodeType !== 1) {
+                    nextSibling = nextSibling.nextSibling;
+                }
+                nextLink = (nextSibling) ? nextSibling.querySelector('.pure-menu-link') : null;
+                // if there is no currently focused link, focus the first one
+                if (!currentLink) {
+                    ddm._menu.querySelector('.pure-menu-link').focus();
+                }
+                else if (nextLink) {
+                    nextLink.focus();
+                }
+            }
+            // Go to the previous link on up arrow
+            else if (ARROW_KEYS_ENABLED && e.keyCode === 38) {
+                /* Up arrow */
+                ddm.halt(e);
+                // get the currently focused link
+                previousSibling = (currentLink) ? currentLink.parentNode.previousSibling : null;
+                while (previousSibling && previousSibling.nodeType !== 1) {
+                    previousSibling = previousSibling.previousSibling;
+                }
+                previousLink = (previousSibling) ? previousSibling.querySelector('.pure-menu-link') : null;
+                // if there is no currently focused link, focus the last link
+                if (!currentLink) {
+                    ddm._menu.querySelector('.pure-menu-item:last-child .pure-menu-link').focus();
+                }
+                // else if there is a previous item, go to the previous item
+                else if (previousLink) {
+                    previousLink.focus();
+                }
+            }
+        });
+
+        // Dismiss an open menu on outside event
+        document.addEventListener(DISMISS_EVENT, function (e) {
+            var target = e.target;
+            if (target !== ddm._link && !ddm._menu.contains(target)) {
+                ddm.hide();
+                ddm._link.blur();
+            }
+        });
+
+    }
+
+    function initDropdowns() {
+        var dropdownParents = document.querySelectorAll('.pure-menu-has-children');
+        for (var i = 0; i < dropdownParents.length; i++) {
+            var ddm = new PureDropdown(dropdownParents[i]);
+        }
+    }
+
+    initDropdowns();
+
+}(this, this.document));
--- js/track-downloads.js
+++ js/track-downloads.js
@@ -1,1 +1,30 @@
-!function(n,e){(n.Pure||(n.Pure={})).trackDownload=function(e,o){var r=n.ga,i=this;if(!r)return;if(r("send","event",e,"download",o),!t)return setTimeout((function(){n.location.href=i.href}),100),!1};var t="download"in e.createElement("a")}(this,this.document);
+(function (window, document) {
+
+    // Expose as `Pure.trackDownload()`
+    (window.Pure || (window.Pure = {})).trackDownload = trackDownload;
+
+    var HAS_DOWNLOAD_ATTR = 'download' in document.createElement('a');
+
+    function trackDownload(category, name) {
+        var ga   = window.ga,
+            link = this;
+
+        // Quit early if Google Analytics isn't on the page.
+        if (!ga) { return; }
+
+        // Fire off the GA ping to track the download event.
+        ga('send', 'event', category, 'download', name);
+
+        // If the browser does not support the HTML5 `download` attribute give
+        // the GA ping some time to complete before starting the download.
+        if (!HAS_DOWNLOAD_ATTR) {
+            setTimeout(function () {
+                window.location.href = link.href;
+            }, 100);
+
+            // Prevent default.
+            return false;
+        }
+    }
+
+}(this, this.document));
